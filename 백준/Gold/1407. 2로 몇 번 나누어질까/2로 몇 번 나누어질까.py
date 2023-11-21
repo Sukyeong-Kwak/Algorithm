760429c0f03ee0aa68dbd50fd2dc4fca2fa70742
sukk <114588184+Sukyeong-Kwak@users.noreply.github.com>
2023-11-21 13:47:20 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.py
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.py
@@ -1,16 +1,11 @@
 def numOfTwo(n):
+    result = n
     twos = 2
     while (twos <= n):
+        result += (n // twos) * (twos//2)
         twos *= 2
-    
-    result = 0
-    nums = 0
-    while (twos >= 1):
-        nums= n // twos - n // (twos*2)
-        result += nums*twos 
-        twos //= 2
     return result
 
 A, B = map(int, input().split())
 
-print(numOfTwo(B)-numOfTwo(A-1))
+print(numOfTwo(B) - numOfTwo(A-1))
