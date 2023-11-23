ea3b824dd15d3fbbd9e13a0c5b88429d5c6e7741
sukk <114588184+Sukyeong-Kwak@users.noreply.github.com>
2023-11-23 15:44:41 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+def recur(number):
+    if (number == M):
+        print(*arr)
+        return
+    
+    for i in range(1, N+1):
+        if len(arr) > 0 and i < arr[-1]:
+            continue
+        arr.append(i)
+        recur(number+1)
+        arr.pop()
+    
+
+N, M = map(int, input().split())
+arr = []
+
+recur(0)
