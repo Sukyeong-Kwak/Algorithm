c50cce2a9a59ef1b334293b22606588c4804acba
sukk <114588184+Sukyeong-Kwak@users.noreply.github.com>
2023-12-20 22:00:57 +0900

    [Gold II] Title: 재활용 캠페인, Time: 168 ms, Memory: 45108 KB -BaekjoonHub

---
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
@@ -0,0 +1,26 @@
+n, x = map(int, input().split())
+
+arr = sorted(list(map(int, input().split())))
+
+s = 0
+e = n-1
+cnt = 0
+remain = 0
+
+while s <= e:
+    if arr[e] >= x:
+        cnt += 1; e -= 1;
+        continue
+    
+    if s == e:
+        remain += 1
+        break;
+    
+    if arr[s] + arr[e] >= x/2:
+        cnt += 1; s += 1; e -= 1;
+    else: 
+        remain += 1; s += 1;
+
+    
+    
+print(cnt + remain//3)
