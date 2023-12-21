29bd14e02c9807f0d4aee36c519e83fef2545768
sukk <114588184+Sukyeong-Kwak@users.noreply.github.com>
2023-12-21 09:54:37 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def recur(node): 
+    
+    visited[node] = 1
+    for nxt in graph[node]:
+        if visited[nxt] == 1:
+            continue
+        
+        recur(nxt)
+        
+recur(1)
+    
+print(sum(visited)-1)
